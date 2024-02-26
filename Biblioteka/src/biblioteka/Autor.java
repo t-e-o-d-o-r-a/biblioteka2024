c9b481446c975941904506e9addd446e59434c47
teodora <teavuckovic17@gmail.com>
2024-02-26 14:53:48 +0100

    Uvedene toString, hashCode i equals metoda u klase Autor i Knjiga

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -1,5 +1,7 @@
 package biblioteka;
 
+import java.util.Objects;
+
 public class Autor {
 
 	private String ime;
@@ -16,6 +18,25 @@ public class Autor {
 	}
 	public void setPrezme(String prezme) {
 		this.prezme = prezme;
+	}
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezme=" + prezme + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(ime, prezme);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(ime, other.ime) && Objects.equals(prezme, other.prezme);
 	}
 	
 	
