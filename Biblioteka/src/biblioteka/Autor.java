60417cb5bf85af55fd80cd452c48e9e25b177a70
teodora <teavuckovic17@gmail.com>
2024-02-26 14:39:17 +0100

    Uvedena klasa Autor, promenjena Knjiga sa listom Autora

---
+++ Biblioteka/src/biblioteka/Autor.java
@@ -0,0 +1,23 @@
+package biblioteka;
+
+public class Autor {
+
+	private String ime;
+	private String prezme;
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezme() {
+		return prezme;
+	}
+	public void setPrezme(String prezme) {
+		this.prezme = prezme;
+	}
+	
+	
+	
+}
